aa43b2a221840cca0d32a030d736c7fe2bc29b07
Harvey Sanders <sanders.harvey@gmail.com>
2017-10-13 16:46:51 -0500

    (feat) Sockets partially set up Add spectator view

--- src/socket/index.js
+++ src/socket/index.js
@@ -1,11 +1,42 @@
+const { processScore } = require('../helpers/game.js');
+
 module.exports = function socketHandler(io) {
-  io.on('connect', function (socket) {
-    socket.emit('news', { hello: 'world' });
-    
-    socket.on('game', handleGame);
+  const players = [];
+  const game = io.of('/game');
+  game.on('connect', function (socket) {
+    socket.join('gameRoom');
+    console.log('connected  !!');
+    console.log('socket ID', socket.id);
+    console.log('socket rooms', socket.rooms);
+
+    socket.on('game', handleGame.bind(null, socket));
+    socket.on('score update', broadcastScore.bind(null, socket));
+    socket.on('spectator join', sendPlayers.bind(null, socket, players));
   });
+  
+  game.on('disconnect', (socket) => {
+    console.log('disconnected', socket.id);
+  });
+
+  const handleGame = (socket, { message, playerId }) => {
+    console.log('handling game');
+    addPlayer(players, playerId);
+    sendPlayers(socket, players);
+  };
+
+  const broadcastScore = (socket, { playerId, score }) => {
+    console.log('broacasting score');
+    socket.to('gameRoom').emit('score update', { playerId, score: processScore(score) });
+  };
+
+  const sendPlayers = (socket, players) => {
+    console.log('sending players');
+    io.sockets.in('gameRoom').emit('player join', { players });
+  };
+
 };
 
-const handleGame = (data) => {
-  console.log(data);
-};
+function addPlayer(players, newPlayer) {
+  !players.includes(newPlayer) && players.push(newPlayer);
+}
+
