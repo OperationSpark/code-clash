3aeef80f1b0988a3bb4383593d9ca24a0302b1c8
Olivia Sabo-Rush <w.phrush@gmail.com>
2017-10-17 14:57:28 -0500

    begin spash page styling

---
+++ app/SplashPageAnimation.jsx
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import Flair from './Spectate/Flair';
+import * as Emojione from 'react-svg-emojione';
+
+class SplashPageAnimation extends Component {
+  constructor() {
+    super();
+  }
+
+  render() {
+    const { player, score, name, emoji } = this.props;
+    return (
+      <div className="row">
+        <div className="emoji col-sm-6">
+          <Emojione.smirk />
+        </div>
+        <div className="emoji right-head col-sm-6">
+          <Emojione.smirk />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default SplashPageAnimation;
