4567ace82003075a6d4244d0ffed761974b949b5
Harvey Sanders <sanders.harvey@gmail.com>
2017-10-17 22:36:38 -0500

    (chore) Switch to CommonJS

--- app/helpers/index.js
+++ app/helpers/index.js
@@ -1,8 +1,15 @@
-export const calcScore = (passCount = 0, failCount = 0) => passCount / (passCount + failCount) || 0;
+const calcScore = (passCount = 0, failCount = 0) => passCount / (passCount + failCount) || 0;
 
-export const renderIf = condition => component => condition && component;
+const renderIf = condition => component => condition && component;
 
-export const randomElement = array => array[Math.floor(Math.random() * array.length)];
+const randomElement = array => array[Math.floor(Math.random() * array.length)];
 
-export const getRandomLine = (str = '') =>
+const getRandomLine = (str = '') =>
   randomElement(str.split('\n').map(s => s.trim()).filter(s => s.length));
+
+module.exports = {
+  calcScore,
+  renderIf,
+  randomElement,
+  getRandomLine
+};
