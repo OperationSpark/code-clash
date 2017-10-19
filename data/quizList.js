e2dd0537145d4395d422c2327924f5054163bb81
Harvey Sanders <sanders.harvey@gmail.com>
2017-10-19 11:23:19 -0500

    (chore) update Quiz list

--- data/quizList.js
+++ data/quizList.js
@@ -1,13 +1,12 @@
 module.exports = [
   '/code-quiz-bootcamp/checkpoints/javascript/checkpoint1/v2/',
-  // '/code-quiz-immersion-prep/capitalizeFirst/',
-  // TODO figure out why capFirst makes the quiz width shrink
-  // '/code-quiz-immersion-prep/countOccurences/',
-  // '/code-quiz-prep/final/',
-  // '/code-quiz-exercises/codeClashQuiz',
-  // '/code-quiz-exercises/every',
-  // '/code-quiz-exercises/range',
-  // '/code-quiz-exercises/sum',
-  // '/code-quiz-exercises/sumBelow',
+  '/code-quiz-immersion-prep/capitalizeFirst/',
+  '/code-quiz-immersion-prep/countOccurences/',
+  '/code-quiz-prep/final/',
+  '/code-quiz-exercises/codeClashQuiz',
+  '/code-quiz-exercises/every',
+  '/code-quiz-exercises/range',
+  '/code-quiz-exercises/sum',
+  '/code-quiz-exercises/sumBelow',
 
 ];
